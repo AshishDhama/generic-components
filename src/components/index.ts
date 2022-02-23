c558b8275ce0bbb5a834b2d91218ef54706b43fe
Ashish Dhama <ashishdhama3@gmail.com>
2022-02-24 00:43:01 +0530

    intial setup

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+
+export {default as TourTip } from './tour_tip';
---
+++ src/components/tour_tip/hooks.ts
@@ -0,0 +1,91 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import {useEffect, useLayoutEffect, useMemo, useRef, useState} from 'react';
+import throttle from 'lodash/throttle';
+
+import {TourTipOverlayPunchOut} from './tour_tip_backdrop';
+
+type PunchOutOffset = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export default function useElementAvailable(
+    elementIds: string[],
+    intervalMS = 250,
+): boolean {
+    const checkAvailableInterval = useRef<NodeJS.Timeout | null>(null);
+    const [available, setAvailable] = useState(false);
+    useEffect(() => {
+        if (available) {
+            if (checkAvailableInterval.current) {
+                clearInterval(checkAvailableInterval.current);
+                checkAvailableInterval.current = null;
+            }
+            return;
+        } else if (checkAvailableInterval.current) {
+            return;
+        }
+        checkAvailableInterval.current = setInterval(() => {
+            if (elementIds.every((x) => document.getElementById(x))) {
+                setAvailable(true);
+                if (checkAvailableInterval.current) {
+                    clearInterval(checkAvailableInterval.current);
+                    checkAvailableInterval.current = null;
+                }
+            }
+        }, intervalMS);
+    }, []);
+
+    return useMemo(() => available, [available]);
+}
+
+
+export const useMeasurePunchouts = (elementIds: string[], additionalDeps: any[], offset?: PunchOutOffset): TourTipOverlayPunchOut | null => {
+    const elementsAvailable = useElementAvailable(elementIds);
+    const [size, setSize] = useState({x: window.innerWidth, y: window.innerHeight});
+    const updateSize = throttle(() => {
+        setSize({x: window.innerWidth, y: window.innerHeight});
+    }, 100, {trailing: true});
+
+    useLayoutEffect(() => {
+        window.addEventListener('resize', updateSize);
+        return () =>
+            window.removeEventListener('resize', updateSize);
+    }, []);
+
+    const channelPunchout = useMemo(() => {
+        let minX = Number.MAX_SAFE_INTEGER;
+        let minY = Number.MAX_SAFE_INTEGER;
+        let maxX = Number.MIN_SAFE_INTEGER;
+        let maxY = Number.MIN_SAFE_INTEGER;
+        for (let i = 0; i < elementIds.length; i++) {
+            const rectangle = document.getElementById(elementIds[i])?.getBoundingClientRect();
+            if (!rectangle) {
+                return null;
+            }
+            if (rectangle.x < minX) {
+                minX = rectangle.x;
+            }
+            if (rectangle.y < minY) {
+                minY = rectangle.y;
+            }
+            if (rectangle.x + rectangle.width > maxX) {
+                maxX = rectangle.x + rectangle.width;
+            }
+            if (rectangle.y + rectangle.height > maxY) {
+                maxY = rectangle.y + rectangle.height;
+            }
+        }
+
+        return {
+            x: `${minX + (offset ? offset.x : 0)}px`,
+            y: `${minY + (offset ? offset.y : 0)}px`,
+            width: `${(maxX - minX) + (offset ? offset.width : 0)}px`,
+            height: `${(maxY - minY) + (offset ? offset.height : 0)}px`,
+        };
+    }, [...elementIds, ...additionalDeps, size, elementsAvailable]);
+    return channelPunchout;
+};
